0d7b9b1b72a5b521eb4e58e6fee623fc3c76f2a3
Daniel Alves Moura <danielalvesmoura2006@gmail.com>
2025-05-09 22:08:54 -0300

    Delete Projeto directory

--- Projeto/src/Impressao.java
+++
@@ -1,9 +0,0 @@
-public class Impressao {
-
-    public void imprimirVetor(int[] vetor) {
-        for (int j = 0; j < vetor.length; j++) {
-            System.out.print(vetor[j] + " ");
-        }
-    }
-
-}
