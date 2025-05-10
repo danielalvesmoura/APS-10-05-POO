4836cfc1e221594a96e98a89c14bd5f62855c10b
Daniel Alves Moura <danielalvesmoura2006@gmail.com>
2025-05-09 22:27:29 -0300

    Update Impressao.java

--- src/global/Impressao.java
+++ src/global/Impressao.java
@@ -8,4 +8,10 @@ public class Impressao {
         }
     }
 
+    public static void imprimirVetorString(String[] vetor) {
+        for (int j = 0; j < vetor.length; j++) {
+            System.out.print(vetor[j] + " ");
+        }
+    }
+
 }
