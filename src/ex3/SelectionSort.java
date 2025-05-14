bab5abf75c6c4d13d147f195d4165dd214f81f12
Daniel Alves Moura <danielalvesmoura2006@gmail.com>
2025-05-14 15:39:41 -0300

    Update SelectionSort.java

--- src/ex3/SelectionSort.java
+++ src/ex3/SelectionSort.java
@@ -3,18 +3,17 @@ package ex3;
 public class SelectionSort {
 
     public void string(String[] vetor) {
+        int menorIndice;
         for (int atual = 0; atual < vetor.length-1; atual++) {
-            for (int i = atual+1; i < vetor.length; i++) {
-                if(vetor[i].compareTo(vetor[atual]) > 0) {
-                    String aux = vetor[i];
-                    vetor[i] = vetor[atual];
-                    vetor[atual] = aux;
+            menorIndice = atual;
+            for (int i = atual + 1; i < vetor.length; i++) {
+                if (vetor[i].compareTo(vetor[menorIndice]) > 0) {
+                    menorIndice = i;
                 }
             }
-            for (int j = 0; j < vetor.length; j++) {
-                System.out.print(vetor[j] + " ");
-            }
-            System.out.println();
+            String aux = vetor[atual];
+            vetor[atual] = vetor[menorIndice];
+            vetor[menorIndice] = aux;
         }
     }
 
