847fc20152e2b2449c8684a4161b5a990b8739aa
Daniel Alves Moura <danielalvesmoura2006@gmail.com>
2025-05-14 15:41:50 -0300

    Update SelectionSort.java

--- src/ex3/SelectionSort.java
+++ src/ex3/SelectionSort.java
@@ -7,7 +7,7 @@ public class SelectionSort {
         for (int atual = 0; atual < vetor.length-1; atual++) {
             menorIndice = atual;
             for (int i = atual + 1; i < vetor.length; i++) {
-                if (vetor[i].compareTo(vetor[menorIndice]) > 0) {
+                if (vetor[i].compareTo(vetor[menorIndice]) < 0) {
                     menorIndice = i;
                 }
             }
