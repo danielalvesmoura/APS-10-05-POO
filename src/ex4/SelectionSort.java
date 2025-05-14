c1c4aa4fce82b1aa54d96bc40d86b89479cb515b
Daniel Alves Moura <danielalvesmoura2006@gmail.com>
2025-05-14 15:42:49 -0300

    Update SelectionSort.java

--- src/ex4/SelectionSort.java
+++ src/ex4/SelectionSort.java
@@ -3,19 +3,18 @@ package ex4;
 public class SelectionSort {
 
 
-    public void padrao(int[] vetor, int n) {
+    public static void padrao(int[] vetor, int n) {
+        int menorIndice;
         for (int atual = 0; atual < n-1; atual++) {
-            for (int i = atual+1; i < n; i++) {
-                if(vetor[i] < vetor[atual]) {
-                    int aux = vetor[i];
-                    vetor[i] = vetor[atual];
-                    vetor[atual] = aux;
+            menorIndice = atual;
+            for (int i = atual + 1; i < n; i++) {
+                if (vetor[i] < vetor[menorIndice]) {
+                    menorIndice = i;
                 }
             }
-            for (int j = 0; j < n; j++) {
-                System.out.print(vetor[j] + " ");
-            }
-            System.out.println();
+            int aux = vetor[atual];
+            vetor[atual] = vetor[menorIndice];
+            vetor[menorIndice] = aux;
         }
     }
 
