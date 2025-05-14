a82fbcca43f949151773b2f6e2d9085f1a67f82d
Daniel Alves Moura <danielalvesmoura2006@gmail.com>
2025-05-14 15:33:58 -0300

    Update SelectionSort.java

--- src/ex2/SelectionSort.java
+++ src/ex2/SelectionSort.java
@@ -3,17 +3,17 @@ package ex2;
 public class SelectionSort {
 
     public void padrao(int[] vetor) {
-        int menorIndice;
+        int maiorIndice;
         for (int atual = 0; atual < vetor.length-1; atual++) {
             maiorIndice = atual;
             for (int i = atual + 1; i < vetor.length; i++) {
-                if (vetor[i] > vetor[menorIndice]) {
+                if (vetor[i] > vetor[maiorIndice]) {
                     maiorIndice = i;
                 }
             }
             int aux = vetor[atual];
-            vetor[atual] = vetor[menorIndice];
-            vetor[menorIndice] = aux;
+            vetor[atual] = vetor[maiorIndice];
+            vetor[maiorIndice] = aux;
         }
     }
 
