91a393acb88df553b67e022ae434ccae1ebd2315
Daniel Alves Moura <danielalvesmoura2006@gmail.com>
2025-05-14 15:47:24 -0300

    Update SelectionSort.java

--- src/ex5/SelectionSort.java
+++ src/ex5/SelectionSort.java
@@ -3,19 +3,20 @@ package ex5;
 public class SelectionSort {
 
 
-    public void padrao(int[] vetor) {
-        for (int atual = 0; atual < vetor.length-1; atual++) {
-            for (int i = atual+1; i < vetor.length; i++) {
-                if(vetor[i] < vetor[atual]) {
-                    int aux = vetor[i];
-                    vetor[i] = vetor[atual];
-                    vetor[atual] = aux;
+    public void padrao(int[][] vetor) {
+        for (int camada = 0; camada < vetor.length; camada++) {
+            int menorIndice;
+            for (int atual = 0; atual < vetor[camada].length-1; atual++) {
+                menorIndice = atual;
+                for (int i = atual + 1; i < vetor[camada].length; i++) {
+                    if (vetor[camada][i] < vetor[camada][menorIndice]) {
+                        menorIndice = i;
+                    }
                 }
+                int aux = vetor[camada][atual];
+                vetor[camada][atual] = vetor[camada][menorIndice];
+                vetor[camada][menorIndice] = aux;
             }
-            for (int j = 0; j < vetor.length; j++) {
-                System.out.print(vetor[j] + " ");
-            }
-            System.out.println();
         }
     }
 
