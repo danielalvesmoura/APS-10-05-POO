c76622db257d44e8f7f1a697f613229f1b96f8c4
Daniel Alves Moura <danielalvesmoura2006@gmail.com>
2025-05-15 10:04:41 -0300

    Update Main.java

--- src/ex8/Main.java
+++ src/ex8/Main.java
@@ -11,6 +11,8 @@ public class Main {
     public static void main(String[] args) {
         Main main = new Main();
 
+        int[] vetorInt = {6,3,7,34,6,3,7,3,6,87,6};
+
         Torneio torneio1 = main.geraTorneio();
 
         torneio1.iniciaTorneio();
@@ -18,15 +20,15 @@ public class Main {
         for (int i = 0; i < torneio1.vetores.length; i++) {
             System.out.printf("\nVetor %d: ",i);
             Impressao.imprimirVetor(torneio1.vetores[i].vetor);
-            System.out.println();
-            System.out.printf("Bubble: %d trocas.\n",torneio1.vetores[i].bubbleIteracoes);
-            System.out.printf("Insertion: %d trocas.\n",torneio1.vetores[i].insertionIteracoes);
-            System.out.printf("Selection: %d trocas.\n",torneio1.vetores[i].selectionIteracoes);
-            System.out.printf("Merge: %d trocas.\n",torneio1.vetores[i].mergeIteracoes);
-
+            System.out.println("Tempos:");
+            System.out.printf("Bubble: %d\n",torneio1.vetores[i].tempos[0]);
+            System.out.printf("Insertion: %d\n",torneio1.vetores[i].tempos[1]);
+            System.out.printf("Selection: %d\n",torneio1.vetores[i].tempos[2]);
+            System.out.printf("Merge: %d\n",torneio1.vetores[i].tempos[3]);
         }
 
     }
+
 
     public Torneio geraTorneio() {
         System.out.println("Quantidade de vetores?");
