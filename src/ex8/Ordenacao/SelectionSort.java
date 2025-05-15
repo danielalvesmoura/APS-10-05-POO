075f2e044bda5d7dd494e5e3f8e84cf988b3a472
Daniel Alves Moura <danielalvesmoura2006@gmail.com>
2025-05-15 10:05:20 -0300

    Update SelectionSort.java

--- src/ex8/Ordenacao/SelectionSort.java
+++ src/ex8/Ordenacao/SelectionSort.java
@@ -6,10 +6,14 @@ import global.Impressao;
 public class SelectionSort {
 
 
-    public int padrao(int[] vetorOriginal) {
+    public void padrao(int[] vetorOriginal) {
         int[] vetor = Arrays.copyOf(vetorOriginal,vetorOriginal.length);
         int menorIndice;
         for (int atual = 0; atual < vetor.length-1; atual++) {
+            if(Global.estaOrdenado(vetor)) {
+                return;
+            }
+
             menorIndice = atual;
             for (int i = atual + 1; i < vetor.length; i++) {
                 if (vetor[i] < vetor[menorIndice]) {
