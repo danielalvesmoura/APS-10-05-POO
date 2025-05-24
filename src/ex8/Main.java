b3de079531aadd63b34f0a90e39846925cac5657
Daniel Alves Moura <danielalvesmoura2006@gmail.com>
2025-05-23 21:44:28 -0300

    Update Main.java

--- src/ex8/Main.java
+++ src/ex8/Main.java
@@ -20,11 +20,10 @@ public class Main {
         for (int i = 0; i < torneio1.vetores.length; i++) {
             System.out.printf("\nVetor %d: ",i);
             Impressao.imprimirVetor(torneio1.vetores[i].vetor);
-            System.out.println("Tempos:");
-            System.out.printf("Bubble: %d\n",torneio1.vetores[i].tempos[0]);
-            System.out.printf("Insertion: %d\n",torneio1.vetores[i].tempos[1]);
-            System.out.printf("Selection: %d\n",torneio1.vetores[i].tempos[2]);
-            System.out.printf("Merge: %d\n",torneio1.vetores[i].tempos[3]);
+            System.out.printf("Bubble: %d nanossegundos\n",torneio1.vetores[i].tempos[0]);
+            System.out.printf("Insertion: %d nanossegundos\n",torneio1.vetores[i].tempos[1]);
+            System.out.printf("Selection: %d nanossegundos\n",torneio1.vetores[i].tempos[2]);
+            System.out.printf("Merge: %d nanossegundos\n",torneio1.vetores[i].tempos[3]);
         }
 
     }
